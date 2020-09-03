de143ec5e5c560ec90043e4756b18386a535b031
Zac Lang <zac.wr.lang@gmail.com>
2020-09-03 16:36:03 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Next.JS + Lambda@Edge
+
+
+```
+yarn dev
+```
+
+```
+yarn deploy
+```
+
+TODO:
+- [] store `.serverless/*` [state in s3](https://gist.github.com/hadynz/`b4e190e0ce10e5811cb462920a9c678f), fetch, update during deploy
+- [] make route53 custom domain work
---
+++ serverless.js
@@ -0,0 +1,13 @@
+const { stage = 'dev' } = require('minimist')(process.argv.slice(2));
+const appName = `zaclang-serverless-next-${stage}`;
+
+module.exports[appName] = {
+  component: "@sls-next/serverless-component@1.16.0-alpha.1",
+  inputs: {
+    runtime: "nodejs12.x",
+    logLambdaExecutionTimes: true,
+    domain: ["app", "zaclang.com"], //[subdomain, domain],
+    bucketName: appName,
+    verbose: true
+  }
+};
