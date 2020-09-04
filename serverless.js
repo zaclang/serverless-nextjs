9f595c8676106377fdcdfe6b74869158e4285ac0
Zac Lang <zac.wr.lang@gmail.com>
2020-09-04 13:41:33 +1000

    removes api

--- serverless.js
+++ serverless.js
@@ -4,8 +4,8 @@ const appName = `zaclang-serverless-next-${stage}`;
 module.exports[appName] = {
   component: "@sls-next/serverless-component@1.16.0-alpha.1",
   inputs: {
-    runtime: "nodejs12.x",
-    logLambdaExecutionTimes: true,
+    // runtime: "nodejs12.x",
+    // logLambdaExecutionTimes: true,
     domain: ["app", "zaclang.com"], //[subdomain, domain],
     bucketName: appName,
     verbose: true
